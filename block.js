b1ad8d7da88acac4165199bf54fa9ea254cc8342
tejaswini9561 <72801807+tejaswini9561@users.noreply.github.com>
2020-12-06 13:08:53 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,36 @@
+class Block {
+    constructor(x, y, width, height) {
+      var options = {
+          'restitution':0.8,
+          'friction':1,
+          'density':1.0
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      
+      this.Visibility = 255;
+
+      World.add(world, this.body);
+    }
+    display(){
+      if(this.body.speed<1.8){
+        var pos =this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        rect(0, 0, this.width, this.height);
+        pop();
+      }
+      else{
+        World.remove(world,this.body);
+        push();
+        this.Visibility = this.Visibility-1;
+        tint(255, this.Visibility);
+        pop();
+      }
+    }
+  };
+  
---
+++ slingshot.js
@@ -0,0 +1,29 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.body = Constraint.create(options);
+        this.pointB = pointB;
+        World.add(world, this.body);
+    }
+    fly(){
+        this.body.bodyA = null;
+    }
+    attach(body){
+        this.body.bodyA= body;
+    }
+    display(){
+        if(this.body.bodyA){
+            var pointA = this.body.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+       
+    }
+    
+}
